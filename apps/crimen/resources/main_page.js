698976c2ea1db9a9b66eef60865ebcfda267e9ce
Martin Kerz <mkerz@me.com>
2010-02-23 09:05:17 +0100

    Ein erster Entwurf der Seitenleiste und Erweiterung der TODOs.

--- apps/crimen/resources/main_page.js
+++ apps/crimen/resources/main_page.js
@@ -25,13 +25,16 @@ Crimen.mainPage = SC.Page.design({
   // The main pane is made visible on screen as soon as your app is loaded.
   // Add childViews to this pane for views to display immediately on page 
   // load.
+  
   mainPane: SC.MainPane.design({
+  
+  //Two childViews in the MainPane: Maincontent and Toolbar
     childViews: 'crimenMaincontent crimenToolbar'.w(),
     
     crimenToolbar: SC.ToolbarView.design({
-      layout: { top: 0, left: 0, right: 0, height: 36 },
-      childViews: 'crimenToolbarHeading crimenToolbarLogoutButton'.w(),
-      anchorLocation: SC.ANCHOR_TOP,
+      layout: { top: 0, left: 0, right: 0, height: 36 }, //Toolbar height set to 36px
+      childViews: 'crimenToolbarHeading crimenToolbarLogoutButton'.w(), //has a heading and a button
+      anchorLocation: SC.ANCHOR_TOP, //sits on top
       crimenToolbarHeading: SC.LabelView.design({
         layout: { centerX: 0, centerY: 0, height: 24, width: 200 },
         controlSize: SC.LARGE_CONTROL_SIZE,
@@ -45,13 +48,51 @@ Crimen.mainPage = SC.Page.design({
       })
     }),
     
-    crimenMaincontent: SC.ScrollView.design({
-      hasHorizontalScroller: NO,
+    crimenMaincontent: SC.SplitView.design({
       layout: { top: 36, bottom: 0, left: 0, right: 0 },
       backgroundColor: 'white',
+      defaultThickness: 200,
  
-      contentView: SC.ListView.design({
+      topLeftView: SC.View.design({
+        layout: { minWidth: 100 },
+        childViews: 'crimenSidebarList crimenSidebarButtons'.w(), //Has the list of entries and the buttons at the bottom
+      
+          crimenSidebarList: SC.ScrollView.design({
+        
+          hasHorizontalScroller: NO, // disable horizontal scrolling
+          backgroundColor: 'lightblue',
+          contentView: SC.LabelView.design({
+            layout: { centerX: 0, centerY: 0, width: 100, height: 18 },
+            tagName: "h1",
+            value: "links" //this is jsut placeholder text. The whole view will be replaced by some sort of ListView
+            })
+          }),
+          
+          crimenSidebarButtons: SC.ToolbarView.design({
+          
+          layout: { bottom: 0, left: 0, right: 0, height: 24 },
+          anchorLocation: SC.ANCHOR_BOTTOM,
+          childViews: 'crimenSidebarButtonsAdd'.w(),
+            crimenSidebarButtonsAdd: SC.ButtonView.design({
+      	
+      	    theme: 'capsule', //What an ugly button. Can anyone figure out how to make this pretty?
+      	    layout: { centerX: 0, centerY: 0, height: 24, width: 100 },
+            title:  "+" 
+            })
+          })
+      }),
+      
+      bottomRightView: SC.ScrollView.design({
+        
+        hasHorizontalScroller: NO, // disable horizontal scrolling
+        contentView: SC.LabelView.design({
+          layout: { centerX: 0, centerY: 0, width: 100, height: 18 },
+          tagName: "h1",
+          value: "rechts" //This text can't be seen, as the text color is white and the background is white, too.
+          })
       })
+      
+      
     })
   }) 
 });
