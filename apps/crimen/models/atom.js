63914bdf23c6d4fb74ec28b02ab1c5ea17f5b9fe
Martin Kerz <mkerz@me.com>
2010-02-24 11:19:26 +0100

    Copyright und Lizenzhinweise ergänzt, Übersetzungen hinzugefügt.

--- apps/crimen/models/atom.js
+++ apps/crimen/models/atom.js
@@ -1,7 +1,22 @@
-// ==========================================================================
-// Project:   Crimen.Atom
-// Copyright: ©2010 My Company, Inc.
-// ==========================================================================
+// =============================================================================
+// Project:   Crimen - Eine konstruktivistische Lernplattform für das Strafrecht
+// Copyright: 2010 Universität des Saarlandes, Lehrstuhl Prof. Dr. Heinz Koriath
+
+// This file is part of crimen.
+
+//    crimen is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+
+//    crimen is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+
+//    You should have received a copy of the GNU General Public License
+//    along with crimen.  If not, see <http://www.gnu.org/licenses/>.
+// =============================================================================
 /*globals Crimen */
 
 /** @class
